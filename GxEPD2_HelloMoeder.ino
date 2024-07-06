e9ffd5d5310bfbfc6d853fc89e294fd601370a51
Selmer van Alten <vanalten@gmail.com>
2024-07-06 11:33:54 +0200

    Added Wifi + NTP

--- GxEPD2_HelloMoeder.ino
+++ GxEPD2_HelloMoeder.ino
@@ -1,55 +1,41 @@
-// GxEPD2_HelloWorld.ino by Jean-Marc Zingg
-//
-// Display Library example for SPI e-paper panels from Dalian Good Display and boards from Waveshare.
-// Requires HW SPI and Adafruit_GFX. Caution: the e-paper panels require 3.3V supply AND data lines!
-//
-// Display Library based on Demo Example from Good Display: https://www.good-display.com/companyfile/32/
-//
-// Author: Jean-Marc Zingg
-//
-// Version: see library.properties
-//
-// Library: https://github.com/ZinggJM/GxEPD2
-
-// Supporting Arduino Forum Topics (closed, read only):
-// Good Display ePaper for Arduino: https://forum.arduino.cc/t/good-display-epaper-for-arduino/419657
-// Waveshare e-paper displays with SPI: https://forum.arduino.cc/t/waveshare-e-paper-displays-with-spi/467865
-//
-// Add new topics in https://forum.arduino.cc/c/using-arduino/displays/23 for new questions and issues
-
-// see GxEPD2_wiring_examples.h for wiring suggestions and examples
-// if you use a different wiring, you need to adapt the constructor parameters!
-
-// uncomment next line to use class GFX of library GFX_Root instead of Adafruit_GFX
-//#include <GFX.h>
-
 #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
 #include <GxEPD2_4C.h>
 #include <GxEPD2_7C.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <WiFi.h>
 
-// select the display class and display driver class in the following file (new style):
+
 #include "GxEPD2_display_selection_new_style.h"
 
-// or select the display constructor line in one of the following files (old style):
-#include "GxEPD2_display_selection.h"
-#include "GxEPD2_display_selection_added.h"
+//Internet
+WiFiUDP ntpUDP;
+int btnGPIO = 0;
+const char SSID[] = "Maaike en Selmer";
+const char password[] = "Wodan Kissa Maaike";
 
-// alternately you can copy the constructor from GxEPD2_display_selection.h or GxEPD2_display_selection_added.h to here
-// e.g. for Wemos D1 mini:
-//GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(/*CS=D8*/ SS, /*DC=D3*/ 0, /*RST=D4*/ 2, /*BUSY=D2*/ 4)); // GDEH0154D67
-
-// for handling alternative SPI pins (ESP32, RP2040) see example GxEPD2_Example.ino
+NTPClient timeClient(ntpUDP);
 
 void setup() {
-  //display.init(115200); // default 10ms reset pulse, e.g. for bare panels with DESPI-C02
   display.init(115200, true, 2, false);  // USE THIS for Waveshare boards with "clever" reset circuit, 2ms reset pulse
+  pinMode(btnGPIO, INPUT);
+  setupWiFi();
+  timeClient.begin();
   helloWorld();
   display.hibernate();
 }
 
-const char HelloWorld[] = "Hello World Moeder!\n\n       Vandaag is het donderdag";
+void setupWiFi() {
+  Serial.begin(115200);
+  WiFi.begin(SSID, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+  delay ( 500 );
+  }
+  
+}
+//const char HelloWorld[] = "Hello World Moeder!\n\n       Vandaag is het donderdag";
 
 void helloWorld() {
   display.setRotation(2);
@@ -57,6 +43,13 @@ void helloWorld() {
   display.setTextColor(GxEPD_RED);
   int16_t tbx, tby;
   uint16_t tbw, tbh;
+  timeClient.update();
+
+  String helloWorld(timeClient.getFormattedTime());
+  
+  char HelloWorld[helloWorld.length()+1];
+  helloWorld.toCharArray(HelloWorld, helloWorld.length());
+
   display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
   // center the bounding box by transposition of the origin:
   uint16_t x = ((display.width() - tbw) / 2) - tbx;
@@ -67,9 +60,10 @@ void helloWorld() {
     display.fillScreen(GxEPD_WHITE);
     display.setCursor(x, y);
     display.setTextColor(GxEPD_RED);
-    display.print(HelloWorld);    
+    display.print(helloWorld);    
     
   } while (display.nextPage());
 }
 
 void loop(){};
+
